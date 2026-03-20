19ab844aa4b39eb90d99dec1580af619e00b0fbd
Leonardo Rocha Vital <leonardorochavital@gmail.com>
2026-03-20 14:19:56 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿double vtotal, vfinal;
+char pagamento, cliente;
+Console.WriteLine("Informe o valor total da compra:");
+vtotal = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Informe D para pagamento em dinheiro ou C para cartão.");
+pagamento = Convert.ToChar(Console.ReadLine().ToUpper());
+
+if (pagamento == 'D')
+{
+    Console.WriteLine("Pagamento em dinheiro.");
+}
+if (pagamento== 'C')
+{
+    Console.WriteLine("Pagamento em cartão.");
+}
+else
+{
+    Console.WriteLine("Opção Inválida. Tente novamente");
+}
+
+
+Console.WriteLine("Informe N para cliente normal ou F para cliente fidelidade.");
+cliente = Convert.ToChar(Console.ReadLine().ToUpper());
+
+if (cliente == 'N')
+{
+    Console.WriteLine("Cliente Normal");
+}
+else if (cliente == 'F')
+{
+    Console.WriteLine("Cliente Fidelidade");
+}
+else
+{
+    Console.WriteLine("Opção Inválida. Tente novamente");
+}
+
+if (pagamento == 'D' && vtotal < 100)
+{
+ vtotal = vfinal * 0.95; // 5% de desconto
+}
