84acb090756201f23e3492c4cce0a61271fc7e6e
Leonardo <leonardorochavital@gmail.com>
2026-03-22 23:25:35 -0300

    projeto atualizado e finalizado

--- Program.cs
+++ Program.cs
@@ -1,8 +1,7 @@
-﻿double vtotal, vfinal;
+﻿double vinicial, vfinal = 0;
 char pagamento, cliente;
 Console.WriteLine("Informe o valor total da compra:");
-vtotal = double.Parse(Console.ReadLine());
-
+vinicial = double.Parse(Console.ReadLine());
 Console.WriteLine("Informe D para pagamento em dinheiro ou C para cartão.");
 pagamento = Convert.ToChar(Console.ReadLine().ToUpper());
 
@@ -10,19 +9,12 @@ if (pagamento == 'D')
 {
     Console.WriteLine("Pagamento em dinheiro.");
 }
-if (pagamento== 'C')
+if (pagamento == 'C')
 {
     Console.WriteLine("Pagamento em cartão.");
 }
-else
-{
-    Console.WriteLine("Opção Inválida. Tente novamente");
-}
-
-
 Console.WriteLine("Informe N para cliente normal ou F para cliente fidelidade.");
 cliente = Convert.ToChar(Console.ReadLine().ToUpper());
-
 if (cliente == 'N')
 {
     Console.WriteLine("Cliente Normal");
@@ -35,8 +27,44 @@ else
 {
     Console.WriteLine("Opção Inválida. Tente novamente");
 }
+double desconto = 0;
 
-if (pagamento == 'D' && vtotal < 100)
+// Desconto baseado no pagamento
+if (pagamento == 'D')
 {
- vtotal = vfinal * 0.95; // 5% de desconto
+    if (vinicial <= 100)
+        desconto += 5; // 5% desconto em dinheiro
+    else
+        desconto += 10; // 10% desconto em dinheiro
 }
+else if (pagamento == 'C')
+{
+    if (vinicial <= 100)
+        desconto += 0; // sem desconto em cartão
+    else if (vinicial <= 300)
+        desconto += 5; // 5% para cartão acima de 100
+    else
+        desconto += 10; // 10% para cartão acima de 300
+}
+
+// Desconto adicional para cliente fidelidade
+if (cliente == 'F' && vinicial > 200)
+{
+    desconto += 5; // 5% adicional
+}
+if (pagamento == 'D' && cliente == 'F')
+{
+    desconto += 2; // 2% de desconto
+}
+if (desconto > 15)
+{
+    desconto = 15;
+}
+vfinal = vinicial * (1 - desconto / 100);
+
+double vdesconto = vinicial - vfinal;
+
+Console.WriteLine("O valor original da sua compra seria de R$ " + vinicial);
+Console.WriteLine("O percentual de desconto aplicado na sua compra é " + desconto + "%");
+Console.WriteLine("O valor do desconto foi R$ " + vdesconto);
+Console.WriteLine("O valor final da sua compra é de R$ " + vfinal);
--- obj/Debug/net10.0/ATP-Trab1.AssemblyInfo.cs
+++ obj/Debug/net10.0/ATP-Trab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ATP-Trab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19ab844aa4b39eb90d99dec1580af619e00b0fbd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ATP-Trab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ATP-Trab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
